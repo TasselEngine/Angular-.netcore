869259a2007213d74686f17075522ea0e377147e
Wallace Sun <miao17game@qq.com>
2017-09-10 19:28:57 +0800

    fix some errors

--- Tassel.angular4/src/app/services/identity/identity.service.ts
+++ Tassel.angular4/src/app/services/identity/identity.service.ts
@@ -1,4 +1,4 @@
-import { LoggerService, Logger } from 'ws-logger';
+import { LogType, LoggerService, Logger } from 'ws-logger';
 import { AsyncableApiServiceBase } from './../base/service.base';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
@@ -18,11 +18,7 @@ export class IdentityService extends AsyncableApiServiceBase {
         super(http);
         this.UserName = 'wallace';
         this.logger = lgsrv.GetLogger(IdentityService).SetModule('service');
-        this.logger.Debug(['title', 'dhjvasdvjsvjsdvjsvd', new Date()], 'this_method');
-        this.logger.Warn(['nothing for', new Date()], '2method');
-        this.logger.Error('nothing for', 'ssgsdrr');
-        this.logger.Info({ name: 'dhvad', age: 50 }, '3mhgd');
-        this.lgsrv.Info({ name: 'dhvad', age: 50 }, '3mhgd');
+        this.logger.Debug(['Identity init.', 'details for development.', { name: 'shabi' }], 'constructor');
     }
 
 }
