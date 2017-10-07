71354eaa54c611183f13220f2cf21f006d31f425
Wallace Sun <miao17game@qq.com>
2017-10-07 17:56:36 +0800

    some edit in register

--- Tassel.angular4/src/app/modules/user/components/register.component.ts
+++ Tassel.angular4/src/app/modules/user/components/register.component.ts
@@ -5,7 +5,13 @@ import { Component, HostBinding, OnInit, Input } from '@angular/core';
 import { pageShowAnimation } from './../../../utils/app.utils';
 import { IdentityService } from '../../../services/app.service';
 import { Regex } from 'ws-regex';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { EqualValidator } from '../../extensions/directives/form_valid.directive';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 interface IUser {
     InputAccount: string;
@@ -32,6 +38,8 @@ export class RegisterComponent extends TasselNavigationBase implements OnInit {
         Remember: true
     };
 
+    private confirm = new Subject<ValidationErrors>();
+
     @HostBinding('@routeAnimation') routeAnimation = true;
     @HostBinding('style.display') display = 'block';
 
@@ -53,6 +61,9 @@ export class RegisterComponent extends TasselNavigationBase implements OnInit {
             recomf: [this.user.ConfirmPsdt, [Validators.required]],
             remember: [this.user.Remember],
         });
+        this.confirm.debounceTime(300).distinctUntilChanged().subscribe(result => {
+            if (result) { this.validateForm.controls.recomf.setErrors(result); }
+        });
     }
 
     prepareSaveModel = () => {
@@ -66,7 +77,16 @@ export class RegisterComponent extends TasselNavigationBase implements OnInit {
         return result;
     }
 
+    public OnPsdInput = () => {
+        this.user.InputPsdt = this.validateForm.value.password;
+    }
+
+    public CheckConfirm = () => {
+        this.confirm.next(EqualValidator(this.user.InputPsdt)(this.validateForm.controls.recomf));
+    }
+
     public SubmitForm = async () => {
+        console.log(this.validateForm.controls.recomf);
         for (const i in this.validateForm.controls) {
             if (i) { this.validateForm.controls[i].markAsDirty(); }
         }
