f8391732688841637a9e8cd873c73a569e27edbe
Wallace Sun <miao17game@qq.com>
2017-10-15 10:30:57 +0800

    fix template lost

--- Tassel.angular4/src/app/modules/admin/root/components/index.component.ts
+++ Tassel.angular4/src/app/modules/admin/root/components/index.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-
+    selector: 'tassel-admin-dash',
+    templateUrl: './../views/dashboard.html',
+    styleUrls: [
+        './../styles/dash.css'
+    ]
 })
 export class DashIndexComponent {
 
