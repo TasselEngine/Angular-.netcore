5a6fb91d3521169dd6cef841b436b14539a5126e
Bruce Wayne <miao17game@qq.com>
2017-11-05 14:42:24 +0800

    edit in animation

--- Tassel.angular4/src/app/utils/animations/page_show.animation.ts
+++ Tassel.angular4/src/app/utils/animations/page_show.animation.ts
@@ -5,9 +5,9 @@ export const pageShowAnimation: AnimationEntryMetadata =
     trigger('routeAnimation', [
         transition(':enter', [
             style({
-                opacity: 0.6,
+                opacity: 0.5,
             }),
-            animate('0.4s ease-in-out', style({ opacity: 1 }))
+            animate('1.2s ease-in-out', style({ opacity: 1 }))
         ]),
         transition(':leave', [
             style({
