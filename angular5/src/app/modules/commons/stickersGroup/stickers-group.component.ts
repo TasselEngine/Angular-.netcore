87d87ede0fc979e30858cdaeb9712d65a11bc0a9
Bruce Wayne <245396954@qq.com>
2018-03-16 20:18:01 +0800

    stickers group upgrade

--- angular5/src/app/modules/commons/stickersGroup/stickers-group.component.ts
+++ angular5/src/app/modules/commons/stickersGroup/stickers-group.component.ts
@@ -1,6 +1,9 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
-import { FormatService, ResourcesService } from '../../../services/app.service';
+import { Component, Output, EventEmitter, Input, OnInit, OnDestroy } from '@angular/core';
+import { FormatService, ResourcesService, IdentityService } from '../../../services/app.service';
 import { ISticker } from '../../../model/app.model';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
     selector: 'tassel-common-stickers',
@@ -9,7 +12,7 @@ import { ISticker } from '../../../model/app.model';
         'stickers-group.scss'
     ]
 })
-export class StickersGroupComponent {
+export class StickersGroupComponent implements OnInit, OnDestroy {
 
     @Input()
     public uploadEnabled = false;
@@ -26,12 +29,66 @@ export class StickersGroupComponent {
 
     public get Formator() { return this.formator; }
 
+    private _caches: { [propName: string]: ISticker[] } = {};
+    private get cache() { return this._caches[this.identity.CurrentUUID] || []; }
+    private set cache(value: ISticker[]) { this._caches[this.identity.CurrentUUID] = value; }
+
+    private colls = [];
+    public get stickers() { return this.colls; }
+
+    private addor = new Subject<ISticker>();
+    private addSubp: Subscription;
+
     constructor(
+        private identity: IdentityService,
         private resources: ResourcesService,
-        private formator: FormatService) { }
+        private formator: FormatService) {
+        this._caches = JSON.parse(window.localStorage.getItem('ws-sticker-caches') || '{}');
+    }
+
+    ngOnInit(): void {
+        this.colls = [
+            { title: 'Tieba', colls: this.TiebaImages, size: 21, cover: this.url('/tieba/middle/25.png') },
+            { title: 'Aru', colls: this.ArusStickers, size: 21, cover: this.url('/aru/asurprised.png') },
+            { title: 'Weibo 01', colls: this.SinaPopStickers, size: 22, cover: this.url('/sina/pop/mogician.png') },
+            { title: 'Weibo 02', colls: this.SinaRoleStickers, size: 22, cover: this.url('/sina/role/ameng-smg.png') },
+            { title: 'Others', colls: this.OthersStickers, size: 22, cover: this.url('/sina/others/doge.png') }
+        ];
+        this.addSubp = this.addor.debounceTime(500).subscribe(image => {
+            this.addStickerCache(image);
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.addSubp.unsubscribe();
+    }
+
+    private url(path: string) {
+        return this.Formator.ImageConnect('/resources/system/images' + path);
+    }
 
     public readonly StickerClicked = (image: ISticker) => {
         this.OnStickerClicked.emit(image);
+        this.addor.next(image);
+    }
+
+    private addStickerCache(image: ISticker) {
+        if (!this.cache) {
+            this._caches[this.identity.CurrentUUID || 'not_login'] = [];
+        }
+        const found = this.cache.findIndex(i => i.key === image.key);
+        if (found >= 0) {
+            this.cache.splice(found, 0);
+            this.cache.unshift(image);
+        } else {
+            this.cache.unshift(image);
+        }
+        this.cache = this.cache.filter((value, index, self) => self.indexOf(value) === index);
+        if (this.cache.length > 24) {
+            this.cache = this.cache.slice(0, 24);
+        }
+        window.localStorage.setItem('ws-sticker-caches', JSON.stringify(this._caches));
+        this.cache = [...this.cache];
     }
 
 }
